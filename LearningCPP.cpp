4ec27b19cc74435b54bc006374602c3f1c32a17b
thisfozz <kfcfozz@vk.com>
2023-09-25 21:09:41 +0500

    Студенты и группа студентов

--- LearningCPP.cpp
+++ LearningCPP.cpp
@@ -72,7 +72,6 @@ public:
     StudentTwo(std::string name, std::string surname, int age, const Date& dateOfBirth, const std::string& phoneNumber)
         : name(name), surname(surname), age(age), date(dateOfBirth), phoneNumber(phoneNumber) {}
 };
-
 class Dot {
 private:
     int x = 0;
@@ -133,7 +132,6 @@ public:
         return std::sqrt(std::pow(dotOther.getX() - this->x, 2) + std::pow(dotOther.getY() - this->y, 2) + std::pow(dotOther.getZ() - this->z, 2));
     }
 };
-
 class NNTName {
 private:
     int valueOne;
@@ -171,6 +169,184 @@ public:
     int minValue() {
         return std::min(valueOne, valueTwo);
     }
+};
+class People {
+public:
+    virtual ~People() {}
+
+    virtual void setName(const std::string& Name) = 0;
+    virtual void setSurname(const std::string& Surname) = 0;
+    virtual void setAge(const int Age) = 0;
+    virtual void setGender(const std::string& Gender) = 0;
+
+    virtual std::string getName() = 0;
+    virtual std::string getSurname() = 0;
+    virtual int getAge() = 0;
+    virtual std::string getGender() = 0;
+
+protected:
+    std::string Name;
+    std::string Surname;
+    int Age;
+    std::string Gender;
+};
+
+class Student : public People {
+public:
+
+    Student() {}
+
+    Student(const std::string& name, const std::string& surname, int age, const std::string& gender, int numberGroup)
+    {
+        setName(name);
+        setSurname(surname);
+        setAge(age);
+        setGender(gender);
+        setNumberGroup(numberGroup);
+    }
+
+    virtual void setName(const std::string& Name) override
+    {
+        this->Name = Name;
+    }
+    virtual void setSurname(const std::string& Surname) override
+    {
+        this->Surname = Surname;
+    }
+    virtual void setAge(const int Age) override
+    {
+        this->Age = Age;
+    }
+    virtual void setGender(const std::string& Gender) override
+    {
+        this->Gender = Gender;
+    }
+    virtual std::string getName() override
+    {
+        return Name;
+    }
+    virtual std::string getSurname() override
+    {
+        return Surname;
+    }
+    virtual int getAge() override
+    {
+        return Age;
+    }
+    virtual std::string getGender() override
+    {
+        return Gender;
+    }
+
+    void setNumberGroup(const int numberGroup) {
+        NumberGroup = numberGroup;
+    }
+    int getNumberGroup() {
+        return NumberGroup;
+    }
+
+private:
+    int AverageRating;
+    int NumberGroup;
+};
+
+class Group {
+public:
+
+    Group() {}
+
+    Group(const std::vector<Student>& initialStudents) : groupStudents(initialStudents) {}
+
+    void Menu() {
+        system("cls");
+        char choice;
+
+        std::cout << "Menu:\n";
+        std::cout << "1. Print Students\n";
+        std::cout << "2. Add Student\n";
+        std::cout << "3. Remove Students\n";
+
+        std::cin >> choice;
+
+        switch (choice) {
+        case '1':
+            PrintStudents();
+            break;
+        case '2':
+            AddStudent();
+            break;
+        case '3':
+            std::cout << "You chose to Remove Students.\n";
+            break;
+        default:
+            std::cout << "Invalid choice. Please enter a number between 1 and 3.\n";
+            break;
+        }
+    }
+private:
+    void AddStudent() {
+        system("cls");
+        std::string Name;
+        std::string Surname;
+        int Age;
+        std::string Gender;
+        int NumberGroup;
+
+        std::cout << "Enter name Student: ";
+        std::cin >> Name;
+
+        std::cout << "\nEnter Surname Student: ";
+        std::cin >> Surname;
+
+        std::cout << "\nEnter Age Student: ";
+        std::cin >> Age;
+
+        std::cout << "\nEnter Gender Student: ";
+        std::cin >> Gender;
+
+        std::cout << "\nEnter Number Group Student: ";
+        std::cin >> NumberGroup;
+
+        Student student(Name, Surname, Age, Gender, NumberGroup);
+        groupStudents.push_back(student);
+
+        system("cls");
+        Menu();
+    }
+
+    void PrintStudents() {
+        system("cls");
+        for (size_t i = 0; i < groupStudents.size(); i++) {
+
+            std::cout << "Student " << i + 1;
+            std::cout << "\nName: " << groupStudents[i].getName();
+            std::cout << "\nSurname: " << groupStudents[i].getSurname();
+            std::cout << "\nAge: " << groupStudents[i].getAge();
+            std::cout << "\nGender: " << groupStudents[i].getGender();
+            std::cout << "\nGroup Number: " << groupStudents[i].getNumberGroup();
+
+            std::cout << "\n\n";
+
+            bool choice;
+            std::cout << "Go back to the menu?";
+            std::cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+                Menu();
+                break;
+            case 2:
+                break;
+            default:
+                break;
+            }
+
+            //Menu();
+        }
+    }
+protected:
+    std::vector<Student> groupStudents;
 };
 
 class Task {
@@ -257,12 +433,18 @@ public:
 
         std::cout << n.MaxValue();
     }
+    void task4() {
+        Group groupStudent;
+        groupStudent.Menu();
+
+    }
 };
 
 int main() {
     Task task;
     //task.task1();
     //task.task2();
-    task.task3();
+    //task.task3();
+    task.task4();
 }
 
